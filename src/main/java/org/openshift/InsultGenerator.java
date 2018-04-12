f5259ef9c1839d1e6c57e36114223d43236ff05b
Vik Hakkal <vhakkal@gmail.com>
2018-04-12 12:09:21 -0400

    test

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -34,6 +34,7 @@ public class InsultGenerator {
 				connection.close();
 			}
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 			return "Database connection problem!";
 		}
 		return theInsult;
