40ad49bd1a9bd7d56c1a737e9debfe739dc8e1bc
Vik Hakkal <vhakkal@gmail.com>
2018-04-12 12:20:09 -0400

    test

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -10,26 +10,25 @@ public class InsultGenerator {
 		String vowels = "AEIOU";
 		String article = "an";
 		String theInsult = "";
+		
 		try {
 			String databaseURL = "jdbc:postgresql://";
 			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
 			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
-			
+
 			String username = System.getenv("POSTGRESQL_USER");
 			String password = System.getenv("PGPASSWORD");
 			Connection connection = DriverManager.getConnection(databaseURL, username, password);
-			
-			if (connection != null) {
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1";
 
+			if (connection != null) {
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
-				
 				while (rs.next()) {
 					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
-					theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
+					theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
 							rs.getString("second"), rs.getString("noun"));
 				}
 				rs.close();
@@ -40,5 +39,4 @@ public class InsultGenerator {
 		}
 		return theInsult;
 	}
-
 }
