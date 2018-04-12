e417a4a6ad290782437d8f5f9890e402d42c3855
Vik Hakkal <vhakkal@gmail.com>
2018-04-12 12:44:11 -0400

    test

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -3,14 +3,15 @@ package org.openshift;
 import java.util.HashMap;
 
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Produces;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @RequestScoped
 @Path("insult")
 public class InsultResource {
 
-	@Get()
+	@GET()
 	@Produces("application/json")
 	public HashMap<String, String> getInsult() {
 		HashMap<String, String> theInsult = new HashMap<>();
